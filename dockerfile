e8d3886457893c9c46db9cd8be6f879dd476fc76
msyc <msyc@sii.pl>
2024-10-23 13:40:25 +0200

    Env problems fixed

--- dockerfile
+++ dockerfile
@@ -1,15 +1,19 @@
 # Use the official GCC image from Docker Hub
 FROM gcc:11.3.0
 
+# Define a build argument to accept the path
+ARG PROJECT_PATH
+
 # Install necessary tools: CMake, Python (for Conan), and Conan
 RUN apt-get update && apt-get install -y \
     cmake \
     python3-pip \
+    make \
     && pip3 install conan \
     && apt-get clean
 
 # Set the working directory inside the container
-WORKDIR /project
+WORKDIR ${PROJECT_PATH}
 
 # Set up Conan profile (relative to the container's /project folder)
 RUN conan profile detect || true
--- start_docker.sh
+++ start_docker.sh
@@ -2,9 +2,14 @@
 
 # Get the directory where the script is located (relative to the script itself)
 SCRIPT_DIR="$(dirname "$0")"
-PROJECT_DIR="$SCRIPT_DIR"
+# PROJECT_DIR="$SCRIPT_DIR"
+PROJECT_DIR="$(pwd)"
+CONAN_CACHE_DIR="$PROJECT_DIR/conan_cache"  # Local directory to store the conan cache from the container
 
 CONTAINER_NAME=cpp_gcc_container
+
+# Create a local conan_cache directory if it doesn't exist
+mkdir -p $CONAN_CACHE_DIR
 
 # Check if the container is already running
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
@@ -15,12 +20,14 @@ else
         echo "Starting existing container $CONTAINER_NAME..."
         docker start $CONTAINER_NAME
     else
+        echo $CONAN_CACHE_DIR
         # If the container doesn't exist, create and start it
         echo "Creating and starting new container $CONTAINER_NAME..."
         docker run -d \
             --name $CONTAINER_NAME \
-            -v $PROJECT_DIR:/project \
-            -w /project \
+            -v $PROJECT_DIR:${PROJECT_DIR} \
+            -v $CONAN_CACHE_DIR:/root/.conan2 \
+            -w ${PROJECT_DIR} \
             cpp_gcc_env tail -f /dev/null
     fi
 fi
